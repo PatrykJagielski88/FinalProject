736a50c31026d9870e966137b5c48a691b2d6147
Patryk Jagielski <pjagielski@academic.rrc.ca>
2022-11-21 19:46:48 -0600

    added cart controller

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,22 @@
+class CartController < ApplicationController
+  def create
+    logger.debug("Adding #{params[:id]} to cart")
+    id = params[:id].to_i
+
+    unless session[:shopping_cart].include?(id)
+      session[:shopping_cart] << id
+      horse = Horse.find(id)
+      flash[:notice] = " #{horse.name} added to cart."
+    end
+
+    redirect_to root_path
+  end
+
+  def destroy
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    horse = Horse.find(id)
+    redirect_to root_path
+    flash[:notice] = " #{horse.name} removed from cart."
+  end
+end

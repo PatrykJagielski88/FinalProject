159ecbbb564ced3c9e7e9757271de64e6a7f8eee
Patryk Jagielski <pjagielski@academic.rrc.ca>
2022-11-22 08:26:57 -0600

    started working on partial view

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -5,8 +5,8 @@ class CartController < ApplicationController
 
     unless session[:shopping_cart].include?(id)
       session[:shopping_cart] << id
-      horse = Horse.find(id)
-      flash[:notice] = " #{horse.name} added to cart."
+      product = Product.find(id)
+      flash[:notice] = " #{product.name} added to cart."
     end
 
     redirect_to root_path
@@ -15,8 +15,8 @@ class CartController < ApplicationController
   def destroy
     id = params[:id].to_i
     session[:shopping_cart].delete(id)
-    horse = Horse.find(id)
+    product = Product.find(id)
     redirect_to root_path
-    flash[:notice] = " #{horse.name} removed from cart."
+    flash[:notice] = " #{product.name} removed from cart."
   end
 end
